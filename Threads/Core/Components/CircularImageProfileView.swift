65bc90f898d162ba19797df680f17e2cc2ea0c24
NilayYadav <nilayyadav10@gmail.com>
2023-10-20 14:12:16 +0530

    ExploreView

---
+++ Threads/Core/Components/CircularImageProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularImageProfileView.swift
+//  Threads
+//
+//  Created by Nilay on 20/10/23.
+//
+
+import SwiftUI
+
+struct CircularImageProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularImageProfileView()
+}
