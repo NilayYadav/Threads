b7916e323d86efedc7297854eba9f96546da7cb7
NilayYadav <nilayyadav10@gmail.com>
2023-10-20 20:35:12 +0530

    Profile UI

--- Threads/Core/Profile/Views/ProfileView.swift
+++ Threads/Core/Profile/Views/ProfileView.swift
@@ -8,8 +8,91 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @State private var selectedFilter: ProfileThreadFilter = .threads
+    @Namespace var animation
+    
+    private var filterBarWidth: CGFloat {
+        let count = CGFloat(ProfileThreadFilter.allCases.count)
+        return UIScreen.main.bounds.width / count - 16
+    }
+    
     var body: some View {
-        Text("Helo, profile")
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 20) {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 12) {
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text("Batman")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                            
+                            Text("Batman")
+                                .font(.subheadline)
+                        }
+                        
+                        Text("I'm Batman")
+                            .font(.footnote)
+                        
+                        Text("2 Followers")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    Spacer()
+                    
+                    CircularImageProfileView()
+                }
+                
+                Button {
+                    
+                } label: {
+                   Text("Follow")
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                        .frame(width: 352, height: 32)
+                        .background(.black)
+                        .cornerRadius(8)
+                }
+                
+                VStack {
+                    HStack {
+                        ForEach(ProfileThreadFilter.allCases) { filter in
+                            VStack {
+                                Text(filter.title)
+                                    .font(.subheadline)
+                                    .fontWeight(selectedFilter == filter ? .semibold : .regular)
+                                
+                                if selectedFilter == filter {
+                                    Rectangle()
+                                        .foregroundColor(.black)
+                                        .frame(width: filterBarWidth, height: 1)
+                                        .matchedGeometryEffect(id: "item", in: animation)
+                                } else {
+                                    Rectangle()
+                                        .foregroundColor(.clear)
+                                        .frame(width: filterBarWidth, height: 1)
+                                }
+                            }
+                            .onTapGesture {
+                                withAnimation(.spring()) {
+                                    selectedFilter = filter
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                LazyVStack {
+                    ForEach (0 ... 10, id: \.self) { thread in
+                       ThreadCell()
+                    }
+                }
+            }
+            .padding(.vertical, 8)
+        }
+        .padding(.horizontal)
     }
 }
 
